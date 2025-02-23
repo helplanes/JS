d8b3284b178c318fee593d33cc1e7328c52f5775
helplanes <helplanes@gmail.com>
2025-02-23 10:47:34 +0530

    learnt variables and datatypes from lovebabbar

--- JS Love Babbar/lec1/script.js
+++ JS Love Babbar/lec1/script.js
@@ -1,3 +1,4 @@
+// var
 // var hello = "hi";
 // console.log(hello);
 
@@ -6,3 +7,17 @@ function solve() {
   console.log(age);
 }
 solve();
+
+// let
+let a = 35;
+console.log(a);
+
+{
+  let a = 45;
+  console.log(a);
+}
+
+// const
+const b = 45;
+console.log(b);
+// b = 24; //error as const can't be reassigned
